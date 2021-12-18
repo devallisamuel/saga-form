daa3de3efa87a723df8d5effdefb1b622da41ccd
devallisamuel <samueltemiloluwaalli@gmail.com>
2021-12-18 04:10:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // dispatch action that stores data in state
+    let form = {
+      name,
+      email,
+      password,
+      phone
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='form'>
+      <form onSubmit = {handleSubmit}>
+        <label>Name:
+        <input type="text"  placeholder='name' onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>Email:
+        <input type="text"  placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+        </label>
+        <label>Password:
+        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder='password'/>
+        </label>Phone:
+        <input type="number" placeholder='number' onChange={(e) => setPhone(e.target.value)}  />
+        <button type='submit'>Submit</button>
+      </form>
+      </div>
     </div>
   );
 }
